510e4230793ca1393c4c3ad0bddb4236666d5699
kdnerd <99752862+root-kdnerd@users.noreply.github.com>
2023-02-14 12:57:03 +0500

    Initial Code Task 3

---
+++ Task-3/Task-3.ts
@@ -0,0 +1,4 @@
+const personName: string = "Raheel";
+console.log(`Lowercase: ${personName.toLowerCase()}`);
+console.log(`Uppercase: ${personName.toUpperCase()}`);
+console.log(`Titlecase: ${personName.charAt(0).toUpperCase()}${personName.slice(1).toLowerCase()}`);
