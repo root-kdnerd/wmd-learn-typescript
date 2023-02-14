64291c0787b9216a4534b404a8cf7eb1961b21c4
kdnerd <99752862+root-kdnerd@users.noreply.github.com>
2023-02-14 13:27:09 +0500

    Task-4 Changing due to Task-5 Similarity

--- Task-4/Task-4.js
+++ Task-4/Task-4.js
@@ -1,3 +1,3 @@
-var author = "Bruce Schneier";
-var Quote = "Amateurs hack systems, professionals hack people";
+var author = "Cory Doctorow";
+var Quote = "Never underestimate the determination of a kid who is time-rich and cash-poor";
 console.log("".concat(author, " once said, \"").concat(Quote, "\""));
--- Task-4/Task-4.ts
+++ Task-4/Task-4.ts
@@ -1,3 +1,3 @@
-const author: string = "Bruce Schneier";
-const Quote: string = "Amateurs hack systems, professionals hack people";
+const author: string = "Cory Doctorow";
+const Quote: string = "Never underestimate the determination of a kid who is time-rich and cash-poor";
 console.log(`${author} once said, "${Quote}"`);
