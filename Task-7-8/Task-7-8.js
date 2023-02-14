463c083b55bca1b5477b0e3a9db2b58364cac0b1
kdnerd <99752862+root-kdnerd@users.noreply.github.com>
2023-02-14 13:51:54 +0500

    Task 7&8 Finalized

---
+++ Task-7-8/Task-7-8.js
@@ -0,0 +1,12 @@
+var addition = "5 + 3";
+var addition_result = 5 + 3;
+var subtraction = "10 - 2";
+var subtraction_result = 10 - 2;
+var multiplication = "4 * 2";
+var multiplication_result = 4 * 2;
+var division = "16 / 2";
+var division_result = 16 / 2;
+console.log("".concat(addition, ": ").concat(addition_result)); // addition
+console.log("".concat(subtraction, ": ").concat(subtraction_result)); // subtraction
+console.log("".concat(multiplication, ": ").concat(multiplication_result)); // multiplication
+console.log("".concat(division, ": ").concat(division_result)); // division
---
+++ Task-7-8/Task-7-8.ts
@@ -0,0 +1,12 @@
+const addition: string = "5 + 3";
+const addition_result = 5+3;
+const subtraction: string = "10 - 2";
+const subtraction_result = 10 - 2
+const multiplication: string = "4 * 2";
+const multiplication_result = 4 * 2;
+const division: string = "16 / 2";
+const division_result = 16 / 2;
+console.log(`${addition}: ${addition_result}`); // addition
+console.log(`${subtraction}: ${subtraction_result}`); // subtraction
+console.log(`${multiplication}: ${multiplication_result}`); // multiplication
+console.log(`${division}: ${division_result}`); // division
