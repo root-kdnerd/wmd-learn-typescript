0a16b326ea1ddf869233330c8fe8787a1c816664
kdnerd <99752862+root-kdnerd@users.noreply.github.com>
2023-02-14 13:22:53 +0500

    Task-5 Finalized

---
+++ Task-5/Task-5.js
@@ -0,0 +1,3 @@
+var famous_person = "Bruce Schneier";
+var message = "Amateurs hack systems, professionals hack people";
+console.log("".concat(famous_person, " once said, \"").concat(message, "\""));
---
+++ Task-5/Task-5.ts
@@ -0,0 +1,3 @@
+const famous_person: string = "Bruce Schneier";
+const message: string = "Amateurs hack systems, professionals hack people";
+console.log(`${famous_person} once said, "${message}"`);
