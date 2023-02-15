2cb384325f52aac1b5fdbc4ac18678fab6d86b65
kdnerd <99752862+root-kdnerd@users.noreply.github.com>
2023-02-15 23:24:21 +0500

    Task 11 Finalized

---
+++ Task-11/Task-11.ts
@@ -0,0 +1,5 @@
+const names: string[] = ["Yaser", "Ali", "Bilal", "Zia"];
+// Loop through the array and print each name
+for (let i: number = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
