da9922e2d834ad2c88e92efbf6cda444b92eff75
kdnerd <99752862+root-kdnerd@users.noreply.github.com>
2023-02-15 23:25:15 +0500

    Task-10 n 11

---
+++ Task-10/Task-10.js
@@ -0,0 +1,6 @@
+var num1 = 5;
+var num2 = 7;
+// Calculate the sum of the two numbers
+var sum = num1 + num2;
+// Output the result to the console
+console.log("The sum of ".concat(num1, " and ").concat(num2, " is ").concat(sum, "."));
---
+++ Task-11/Task-11.js
@@ -0,0 +1,5 @@
+var names = ["Yaser", "Ali", "Bilal", "Zia"];
+// Loop through the array and print each name
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
