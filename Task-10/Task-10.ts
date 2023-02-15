e152442fb9061e5c6ea2a87cf564331a520629eb
kdnerd <99752862+root-kdnerd@users.noreply.github.com>
2023-02-15 23:19:26 +0500

    Task-10 finalized

---
+++ Task-10/Task-10.ts
@@ -0,0 +1,6 @@
+const num1: number = 5;
+const num2: number = 7;
+// Calculate the sum of the two numbers
+const sum: number = num1 + num2;
+// Output the result to the console
+console.log(`The sum of ${num1} and ${num2} is ${sum}.`);
