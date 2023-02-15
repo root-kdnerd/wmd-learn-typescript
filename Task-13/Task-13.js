cfeb9cdab1fbace8f82f6bcee8f27d49ca5d2ca5
kdnerd <99752862+root-kdnerd@users.noreply.github.com>
2023-02-15 23:36:30 +0500

    Task 13 Finalized

---
+++ Task-13/Task-13.js
@@ -0,0 +1,5 @@
+var cars = ["Honda Civic", "Toyota Corolla", "Ford Mustang", "BMW M3"];
+// Loop through the array and print a statement about each car
+for (var i = 0; i < cars.length; i++) {
+    console.log("I would like to own a " + cars[i] + ".");
+}
