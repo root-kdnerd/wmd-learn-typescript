935b3d6cae8cdf65728a06156a56e9c380eb40e8
kdnerd <99752862+root-kdnerd@users.noreply.github.com>
2023-02-21 22:57:00 +0500

    Task 28-30 Completed

---
+++ Task-30/Task-30.ts
@@ -0,0 +1,9 @@
+let usernames: string[] = ['john', 'jane', 'admin', 'bob', 'alice'];
+
+for (let username of usernames) {
+  if (username === 'admin') {
+    console.log(`Hello ${username}, would you like to see a status report?`);
+  } else {
+    console.log(`Hello ${username}, thank you for logging in again.`);
+  }
+}
