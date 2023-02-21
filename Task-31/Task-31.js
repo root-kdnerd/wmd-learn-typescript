6d8f859668556995a7b407a8a35f83b4ce07a925
kdnerd <99752862+root-kdnerd@users.noreply.github.com>
2023-02-21 23:00:10 +0500

    Task-31 Completed

---
+++ Task-31/Task-31.js
@@ -0,0 +1,15 @@
+//let usernames: string[] = ['admin', 'john', 'jane', 'alice', 'bob'];
+var usernames = [];
+if (usernames.length > 0) {
+    for (var i = 0; i < usernames.length; i++) {
+        if (usernames[i] === 'admin') {
+            console.log('Hello admin, would you like to see a status report?');
+        }
+        else {
+            console.log("Hello ".concat(usernames[i], ", thank you for logging in again."));
+        }
+    }
+}
+else {
+    console.log('We need to find some users!');
+}
